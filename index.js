fa494c52d962de6c87496022be78bf596a0ebd57
webdacjs <juan@jobspotting.com>
2018-04-13 04:28:07 +0200

    Moving filter out to separate file.

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 const descwords = ['desc', 'reverse']
-
-// Lodash filter function released under MIT license.
-// https://raw.githubusercontent.com/lodash/lodash/4.17.4/LICENSE
-const filter = (array, predicate) => {
-  let index = -1
-  let resIndex = 0
-  const length = array == null ? 0 : array.length
-  const result = []
-
-  while (++index < length) {
-    const value = array[index]
-    if (predicate(value, index, array)) {
-      result[resIndex++] = value
-    }
-  }
-  return result
-}
+const filter = require('./filter.js')
 
 const injMissVals = (originalArray, sortedArray, key) => {
   const miss = filter(originalArray, x => !x[key])
