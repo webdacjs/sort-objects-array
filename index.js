a3ac49525895ec0e8ba6f7596e9a9d1b094b01e7
Juan Convers <me@jco.im>
2019-08-12 21:44:46 +0200

    Improvements in index.js

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const {
 /**
  * Sort an objects array based on the property required in the parameters.
  *
- * @param {Array} arrayToSort - source array
+ * @param {Array} valueToSort - source array or object to sort
  * @param {string} key - name of property to use in the sort.
  * @param {string or object} order or config - optional 'desc' or 'reverse' string
  *      to indicate sort descending or configuration object with order and / or
  *      caseinsensitive flag.
  * @returns {Array}
 */
-const sortObjectsArray = (valueToSort, key, orderOrConfig) => {
+function sortObjectsArray (valueToSort, key, orderOrConfig) {
   const arrayToSort = isAnObject(valueToSort) ? objToArray(valueToSort) : valueToSort
   if (validateArgs(arrayToSort, key)) {
     ch.warn('* sort-objects-array: Wrong arguments returning original array / object')
